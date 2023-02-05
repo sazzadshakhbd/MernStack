ca7f115013a7bf74f6d69c3aae16880305f7dd24
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2023-02-05 19:29:02 +0600

    while and for loop break and continue

--- Milestone-04-Hello-JavaScript/Module-18-Loop-and-Practice-Problems/practice.js
+++ Milestone-04-Hello-JavaScript/Module-18-Loop-and-Practice-Problems/practice.js
@@ -1,5 +1,8 @@
-var items = ['Bottle', 'Mouse', 'Sunglass', 'Pen'];
-for (var i = 0; i < items.length; i++) {
-    var item = items[i];
-    console.log(item);
+var numbers = [45, 87, 89, 56.32, 51, 25, 188, 41, 25, 98];
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    if (number > 50) {
+        continue;
+    }
+    console.log(number);
 }
