cef2507f262a3c737f3fd9ec59bcfca2b4cad152
sazzadshakhbd <sazzadshakhbd@hotmail.com>
2023-02-06 21:51:36 +0600

    Understand for and while loop in a reverse way

--- Milestone-04-Hello-JavaScript/Module-18-Loop-and-Practice-Problems/practice.js
+++ Milestone-04-Hello-JavaScript/Module-18-Loop-and-Practice-Problems/practice.js
@@ -1,8 +1,3 @@
-var numbers = [45, 87, 89, 56.32, 51, 25, 188, 41, 25, 98];
-for (var i = 0; i < numbers.length; i++) {
-    var number = numbers[i];
-    if (number > 50) {
-        continue;
-    }
-    console.log(number);
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
 }
